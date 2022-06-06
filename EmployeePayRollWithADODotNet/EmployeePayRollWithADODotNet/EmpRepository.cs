6df6cf315ad399075703a1e6bc39134e6753adc5
Praan26 <arpanap102@gmail.com>
2022-06-06 21:59:07 +0530

    [Arpan] Connection to data base added

--- EmployeePayRollWithADODotNet/EmployeePayRollWithADODotNet/EmpRepository.cs
+++ EmployeePayRollWithADODotNet/EmployeePayRollWithADODotNet/EmpRepository.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeePayRollWithADODotNet
 {
-    internal class EmpRepository
+    public class EmpRepository
     {
+        private SqlConnection con;    
+        private void connection()
+        {
+            string connectingString = "Data Source=(localdb)\\MSSQLLocaldb;Initial Catalog=Payroll_Service";
+            con = new SqlConnection(connectingString);
+        }
     }
 }
